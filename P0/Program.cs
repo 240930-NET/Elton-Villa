6cb85a840e3443b407958eb441572a3b5d0a8f97
Elton V <43376123+eltonvilla@users.noreply.github.com>
2024-10-02 20:49:17 -0600

    Fixed README, started Program.cs

--- P0/Program.cs
+++ P0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+    public static void Main(string[] args){
+        //Initialize categories
+
+        //Ask user to pick categories
+
+        //Ask user to pick a number
+        Console.WriteLine("Please pick a \"random\" number between 3 and 10");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        //Cross out options
+
+        //Reveal results
+    }
+}
