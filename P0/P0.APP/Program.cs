50d098a07165c4ca84fb9b9c1537da6842ef2b09
Elton V <43376123+eltonvilla@users.noreply.github.com>
2024-10-05 20:26:00 -0600

    Console and Xunit testing setup

---
+++ P0/P0.APP/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace P0.APP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ P0/P0.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace P0.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
